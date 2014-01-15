a8c6651077acca3c308f99273f4da82f56a4f690
Oleg Semenov <izzzum@gmail.com>
2014-01-15 21:20:57 +0200

    Add new wifi module support

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -308,9 +308,9 @@ int wifi_load_driver()
 		property_get("ro.serialno", serialno, "f6r6rd1212541368");
 
 		if(strlen(wlan_mac) == 0)		
-			snprintf(huawei_module_arg, sizeof(huawei_module_arg), "%s wlan_mac=%02x%02x%02x%02x%02x%02x", DRIVER_MODULE_ARG, serialno[0], serialno[1], serialno[2], serialno[3], serialno[4], serialno[5]);
+			snprintf(huawei_module_arg, sizeof(huawei_module_arg), "%s mac_param=%02x:%02x:%02x:%02x:%02x:%02x", DRIVER_MODULE_ARG, serialno[0], serialno[1], serialno[2], serialno[3], serialno[4], serialno[5]);
 		else
-			snprintf(huawei_module_arg, sizeof(huawei_module_arg), "%s wlan_mac=%s", DRIVER_MODULE_ARG, wlan_mac);	
+			snprintf(huawei_module_arg, sizeof(huawei_module_arg), "%s mac_param=%s", DRIVER_MODULE_ARG, wlan_mac);	
 
 		ALOGW("HUAWEI WIFI DRIVER MODULE ARG: %s", huawei_module_arg);
 		if (insmod(DRIVER_MODULE_PATH, huawei_module_arg) < 0) {
