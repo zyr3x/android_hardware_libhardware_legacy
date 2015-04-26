115a2380d6250f7cd963bb2eebcf6cf0f35e3b72
zyr3x <izzzum@gmail.com>
2015-04-26 10:06:39 +0300

    Add Mediapad Wif
    Change-Id: Ic64f52191a6f5067d995936d539c12f99e087e3c

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -48,4 +48,17 @@ ifeq ($(BOARD_HAVE_SAMSUNG_WIFI),true)
 LOCAL_CFLAGS += -DSAMSUNG_WIFI
 endif
 
+ifeq ($(BOARD_HAVE_HUAWEI_WIFI),true)
+    LOCAL_CFLAGS += -DHUAWEI_WIFI
+endif
+
 LOCAL_SHARED_LIBRARIES += libnetutils
+
+# Proprietary
+LOCAL_SHARED_LIBRARIES += liboncrpc
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/liboncrpc_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/liboncrpc_intermediates/export_includes)
+
+LOCAL_SHARED_LIBRARIES += libnv
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libnv_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libnv_intermediates/export_includes)
